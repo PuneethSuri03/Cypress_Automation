9397d88b7c543e46cb7f970becfc2a7d6869a62b
Puneeth.s <Puneeth.s@brillio.com>
2024-05-02 13:53:15 +0530

    new code

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,7 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  defaultCommandTimeout: 30000,
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    testIsolation: false,
-    specPattern: 'cypress/e2e/**/*.js',
-    viewportHeight: 667,
-    viewportWidth: 375,
-    testIsolation: false,
-    responseTimeout: 60000,
-    requestTimeout: 30000
   },
-});
+};
